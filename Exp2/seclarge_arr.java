b4a2de7183be8907860d063e90fa803dd95f5f57
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:30:34 +0530

    Create seclarge_arr.java

---
+++ Exp2/seclarge_arr.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class seclarge_arr
+{
+    public static void main(String args[])
+    {
+        int n,i,l,sl;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array size : ");
+        n = sc.nextInt();
+        
+        int a[] = new int [20];
+        
+        System.out.println("Enter the array elements : ");
+        for(i=0;i<n;i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        
+        l = a[0];
+        sl = a[0];
+        
+        for(i=0;i<n;i++)
+        {
+            if(a[i]>l)
+                l = a[i];
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            if(a[i]>sl && a[i]<l)
+            {
+                sl = a[i];
+            }
+        }
+        System.out.println("Second Largest Element = "+sl);
+    }
+}
