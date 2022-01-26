463407c76fbbc8819078f122dd7f085cea8d3b25
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:16:54 +0530

    Create palindrome.java

---
+++ Exp1/palindrome.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class palindrome
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n,num,rem,sum=0;
+        
+        System.out.println("Enter a number : ");
+        n = sc.nextInt();
+        num = n;
+        
+        while(n!=0)
+        {
+            rem = n%10;
+            sum = sum*10 + rem;
+            n = n/10;
+        }
+        
+        if(sum == num)
+            System.out.println("Palindrome");
+        
+        else
+            System.out.println("Not Plaindrome");
+    }
+}
