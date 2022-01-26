a6ef17fd676be4aebfa4c27a0110a41de4f8e175
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:39:53 +0530

    Create trace_mat.java

---
+++ Exp2/trace_mat.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class trace_mat
+{
+    public static void main(String args[])
+    {
+        int n,i,j,sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of matrix : ");
+        n = sc.nextInt();
+        
+        int a[][] = new int[20][20];
+        
+        System.out.println("Enter the elements of matrix : ");
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==j)
+                {
+                    sum = sum + a[i][j];
+                }
+            }
+        }
+        
+        System.out.println("Trace : "+sum);
+    }
+}
