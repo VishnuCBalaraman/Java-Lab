0e5ce94eb0c6260e96a018c75c61364be77896a6
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:08:00 +0530

    Create sum_nat.java

---
+++ Exp1/sum_nat.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class sum_nat
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n,i,sum=0;
+        
+        System.out.println("Enter a limit : ");
+        n = sc.nextInt();
+        
+        for(i=1;i<=n;i++)
+        {
+            sum = sum + i;
+        }
+        
+        System.out.println("Sum = "+sum);
+    }
+}
