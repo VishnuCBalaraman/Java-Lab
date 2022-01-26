b8896700a8b5feab5a73e13f5af5644a70bbeeb5
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:36:01 +0530

    Create transpose_mat.java

---
+++ Exp2/transpose_mat.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class transpose_mat
+{
+    public static void main(String args[])
+    {
+        int r,c,i,j;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row size : ");
+        r = sc.nextInt();
+        System.out.println("Enter the column size : ");
+        c = sc.nextInt();
+        
+        int a[][] = new int[20][20];
+        
+        System.out.println("Enter the elements of matrix : ");
+        for(i=0;i<r;i++)
+        {
+            for(j=0;j<c;j++)
+            {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        
+        int b[][] = new int[20][20];
+        
+        for(i=0;i<c;i++)
+        {
+            for(j=0;j<r;j++)
+            {
+                b[i][j] = a[j][i];
+            }
+        }
+        
+        System.out.println("Transpose : ");
+        for(i=0;i<c;i++)
+        {
+            for(j=0;j<r;j++)
+            {
+                System.out.print(b[i][j]+" ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
