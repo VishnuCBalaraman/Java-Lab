6f80f4f4dfae7b216aeeedce20a4a678360e5420
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:12:42 +0530

    Create fibonacci.java

---
+++ Exp1/fibonacci.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class fibonacci
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n,i,t1=0,t2=1,next;
+        
+        System.out.println("Enter a limit : ");
+        n = sc.nextInt();
+        
+        System.out.println("Fibinocci Series : ");
+        for(i=1;i<=n;i++)
+        {
+            System.out.print(t1+" ");
+            next = t1+t2;
+            t1 = t2;
+            t2 = next;
+        }
+    }
+}
