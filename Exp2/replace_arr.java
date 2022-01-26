3116829f807cb41917635c08974cabf015d0dcf5
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:28:16 +0530

    Create replace_arr.java

---
+++ Exp2/replace_arr.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class replace_arr 
+{
+    public static void main(String args[])
+    {
+        int n,i,j,num,key;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array size : ");
+        n = sc.nextInt();
+        
+        int a[] = new int[20];
+        
+        System.out.println("Enter the array elements : ");
+        for(i=0;i<n;i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        
+        System.out.println("Select the element : ");
+        num = sc.nextInt();
+        System.out.println("Enter the key element : ");
+        key = sc.nextInt();
+        
+        for(i=0;i<n;i++)
+        {
+            if(a[i] == num)
+            {
+                a[i] = key;
+            }
+        }
+        
+        System.out.println("Output : ");
+        for(i=0;i<n;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+}
