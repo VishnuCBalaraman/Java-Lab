00c360242e363447c26612e34d03c54ba1054a29
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:24:35 +0530

    Create sum_arr.java

---
+++ Exp2/sum_arr.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class sum_arr
+{
+    public static void main(String args[])
+    {
+        int n,i,sum=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array size : ");
+        n = sc.nextInt();
+        
+        int a[] = new int [20];
+        
+        System.out.println("Enter the array elements : ");
+        for(i=0;i<n;i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        
+        for(i=0;i<n;i++)
+        {
+            sum = sum + a[i];
+        }
+        
+        System.out.println("Sum = "+sum);
+    }
+}
