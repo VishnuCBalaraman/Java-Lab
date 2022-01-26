14043cee55592d444f4bf164400adae1ef83b542
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:03:17 +0530

    Create sum.java

---
+++ Exp1/sum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class sum 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b,sum=0;
+        
+        System.out.println("Enter First Number : ");
+        a = sc.nextInt();
+        
+        System.out.println("Enter Second Number : ");
+        b = sc.nextInt();
+        
+        sum = a+b;
+        
+        System.out.println("Sum = "+sum);
+    }
+}
