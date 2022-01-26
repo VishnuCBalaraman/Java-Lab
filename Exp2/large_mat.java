15aaa5db3a5da74186a6700c6f0ec4e7f7e169a0
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:31:57 +0530

    Create large_mat.java

---
+++ Exp2/large_mat.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class large_mat
+{
+    public static void main(String args[])
+    {
+        int r,c,i,j,l;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row size : ");
+        r = sc.nextInt();
+        System.out.println("Enter the column size : ");
+        c = sc.nextInt();
+        
+        int a[][] = new int [20][20];
+        
+        System.out.println("Enter the elements of matrix : ");
+        for(i=0;i<r;i++)
+        {
+            for(j=0;j<c;j++)
+            {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        
+        l = a[0][0];
+        
+        for(i=0;i<r;i++)
+        {
+            for(j=0;j<c;j++)
+            {
+                if(a[i][j] > l)
+                    l = a[i][j];
+            }
+        }
+        
+        System.out.println("Largest Element = "+l);
+    }
+}
