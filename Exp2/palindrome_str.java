ca004a56a241359481561bcbae9e20ce0fe92040
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:41:10 +0530

    Create palindrome_str.java

---
+++ Exp2/palindrome_str.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class palindrome_str
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        
+        System.out.println("Enter a string : ");
+        str = sc.nextLine();
+        
+        String org_str = str;
+        int len = str.length();
+        
+        String rev = "";
+        
+        for(int i=len-1;i>=0;i--)
+        {
+            rev = rev + str.charAt(i);
+        }
+        
+        if(org_str.equals(rev))
+            System.out.println("Plaindrome");
+        
+        else
+            System.out.println("Not Palindrome");
+    }
+}
