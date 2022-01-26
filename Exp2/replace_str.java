e2b9c9fafddb145f53f677ad1dc3bd13f9c167e7
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:44:28 +0530

    Create replace_str.java

---
+++ Exp2/replace_str.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class replace_str
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("Enter a string : ");
+        str = sc.nextLine();
+        
+        char item;
+        System.out.println("Enter a Character from the given string : ");
+        item = sc.next().charAt(0);
+        
+        char key;
+        System.out.println("Enter the key element : ");
+        key = sc.next().charAt(0);
+        
+        str = str.replace(item,key);
+        
+        System.out.print("Output : ");
+        System.out.println(str);
+        
+    }
+}
