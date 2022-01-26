4a9a40d9f5379ca9f13b1bb57db671fa394469d5
VishnuCBalaraman <83954725+VishnuCBalaraman@users.noreply.github.com>
2022-01-26 13:05:53 +0530

    Create prime.java

---
+++ Exp1/prime.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+class prime
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n,i,count=0;
+        
+        System.out.println("Enter a number : ");
+        n = sc.nextInt();
+        
+        for(i=1;i<=n;i++)
+        {
+            if(n%i == 0)
+            {
+                count++;
+            }
+        }
+        
+        if(count==2)
+            System.out.println("Prime");
+        
+        else
+            System.out.println("Not Prime");
+        
+    }
+}
